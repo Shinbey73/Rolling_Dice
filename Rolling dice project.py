5273f2b28b9833f55a93d9db50a03b59e08a5883
Shinbey73 <sufi73kd@gmail.com>
2025-02-15 13:16:39 +0800

    Added condition to check if the user wants to roll the dice again. Added restriction where the user can only enter 'y' or 'n' to roll the dice again.

--- Rolling dice project.py
+++ Rolling dice project.py
@@ -4,19 +4,29 @@ def rolling_dice():
     return random.randint(1,6)
 
 def main():
-    print('===============WELCOME TO THE DICE ROLLING SIMULATOR!================')
+    print('\n===============WELCOME TO THE DICE ROLLING SIMULATOR!================')
     
-    print('Do you want to roll the dice?')
+    while True:
+        print('\nDo you want to roll the dice?\n')
 
-    user_input = input('Enter "yes" or "no": ')
+        try:
+            
+            user_input = input('Enter "y" or "n": ').lower()
 
-    if user_input == 'yes':
-        print('Rolling the dice...')
-        print('The dice shows: ', rolling_dice())
-      
-    elif user_input == 'no':
-        print('Goodbye!')
+            if user_input != 'y' and user_input != 'n':
+                raise error
 
+            if user_input == 'y':
+                print('\nRolling the dice...')
+                print('\nThe dice shows: ', rolling_dice())
+            
+            elif user_input == 'n':
+                print('\nGoodbye!')
+                break
+            
+        except Exception as error:
+            print('\nInvalid input. Please enter "y" or "n" only!.')
+            continue
 
 
 if __name__ == '__main__':
